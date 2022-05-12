8271ee47a8c6098fe7f8e20ca46a22b63282ab11
abhisek102 <abhisek.nanda@contractor.tatacommunications.com>
2022-05-13 01:15:35 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const http=require('http');
+const http = require('http');
+const fs = require('fs');
 
-const server=http.createServer(req,res)=>{
-    res.end("connected");
-}
-server.listen(8000)
+const server = http.createServer((req, res) => {
+  fs.readFile(`emplyees.json`, 'utf-8', (err, data) => {
+    console.log('connected successfully');
+    const employee = JSON.parse(data);
+    res.end(data);
+    console.log(err);
+  });
+});
+
+server.listen(8000);
